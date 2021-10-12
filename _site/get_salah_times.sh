48f3492bc3b94579d60736fc663176d2b5681412
Saad Khan <saad.khan02@gmail.com>
2021-10-12 12:36:16 -0400

    Update iqamah times for 10th Oct

---
+++ _site/get_salah_times.sh
@@ -0,0 +1,4 @@
+curl http://api.aladhan.com/v1/timingsByAddress?address=185%20Guideboard%20Rd,Waterford,NY > _data/adhaan_times.json
+git add _data/adhaan_times.json
+git commit -m "Updated adhaan times"
+git push origin main
